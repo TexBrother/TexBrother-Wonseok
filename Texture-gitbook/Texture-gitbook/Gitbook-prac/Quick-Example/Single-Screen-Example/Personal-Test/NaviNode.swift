ce6ba6967715ee00b5ea3ad47c3adb7f490bdd9d
wonseok <wurijang@gmail.com>
2021-08-11 22:32:20 +0900

    [study] creating NaviNode LayoutSpec

--- Texture-gitbook/Texture-gitbook/Gitbook-prac/Quick-Example/Single-Screen-Example/Personal-Test/NaviNode.swift
+++ Texture-gitbook/Texture-gitbook/Gitbook-prac/Quick-Example/Single-Screen-Example/Personal-Test/NaviNode.swift
@@ -14,6 +14,7 @@ final class NaviNode: ASDisplayNode {
         let node = ASTextNode()
         let paragraphStyle = NSMutableParagraphStyle()
         paragraphStyle.alignment = .center
+        node.backgroundColor = .red
         node.attributedText = NSAttributedString(
             string: "식물 결과",
             attributes: [
@@ -27,6 +28,7 @@ final class NaviNode: ASDisplayNode {
     
     private let backBtn: ASButtonNode = {
         let node = ASButtonNode()
+        node.backgroundColor = .orange
         node.setImage(UIImage(systemName: "arrow.backward"), for: .normal)
         node.addTarget(self, action: #selector(backAction), forControlEvents: .touchUpInside)
         return node
@@ -43,47 +45,28 @@ final class NaviNode: ASDisplayNode {
     // MARK: Node Life Cycle
     override func layout() {
         super.layout()
+        self.backgroundColor = .blue
     }
     
     
     // MARK: Layout
-    
     override func layoutSpecThatFits(_ constraintedSize: ASSizeRange) -> ASLayoutSpec {
-        var containerInsets: UIEdgeInsets = self.safeAreaInsets
-        containerInsets.left += 15.0
-        containerInsets.right += 15.0
-        containerInsets.top = containerInsets.bottom
-        
-        return ASInsetLayoutSpec (
-            insets: containerInsets,
-            child: contentLayoutSpec()
+        let backBtnLayout =  ASCenterLayoutSpec(
+            horizontalPosition: .start,
+            verticalPosition: .center,
+            sizingOption: [],
+            child: self.backBtn
         )
-    }
-    
-    private func contentLayoutSpec() -> ASLayoutSpec {
         let titleLayout =  ASCenterLayoutSpec(
             horizontalPosition: .center,
             verticalPosition: .center,
             sizingOption: [],
             child: self.titleNode
         )
-
-        let backBtnLayout = ASCenterLayoutSpec (
-            horizontalPosition: .start,
-            verticalPosition: .center,
-            sizingOption: [], // 기본
-            child: self.backBtn
-        )
         
-        return ASStackLayoutSpec (
-            direction: .horizontal,
-            spacing: 0.0,
-            justifyContent: .spaceBetween,
-            alignItems: .notSet,
-            children: [
-                backBtnLayout,
-                titleLayout
-            ]
+        return ASAbsoluteLayoutSpec (
+            sizing: .default,
+            children: [backBtnLayout, titleLayout]
         )
     }
 }

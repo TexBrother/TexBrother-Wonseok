ca6d4d2ef8e58134362c2ca5f1eac63abf348d7d
wonseok <wurijang@gmail.com>
2021-09-22 22:25:37 +0900

    [feat] CardCell Data Model 생성

---
+++ StarBucks_Clone/StarBucks_Clone/Model/CardInfo.swift
@@ -0,0 +1,21 @@
+//
+//  CardInfo.swift
+//  StarBucks_Clone
+//
+//  Created by Wonseok Lee on 2021/09/21.
+//
+
+import Foundation
+
+struct CardInfo {
+    let cardImgName: String
+    let name: String
+    let balance: String
+    let barcodeImgName: String
+    let barcodeNum: String
+}
+
+//var CardListData: [CardInfo] = [
+//    CardInfo(cardImgName: "cardSample", name: "카드", balance: "4,000원", barcodeImgName: "bannerSample", barcodeNum: "123132")
+//]
+var CardListData = [CardInfo]()

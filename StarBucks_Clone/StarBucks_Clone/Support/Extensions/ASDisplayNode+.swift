c525eb746df2a941df838e659fe9904342cdfa8b
wonseok <wurijang@gmail.com>
2021-09-29 22:29:09 +0900

    [feat] dropShadow, Radius layer 제거

--- StarBucks_Clone/StarBucks_Clone/Support/Extensions/ASDisplayNode+.swift
+++ StarBucks_Clone/StarBucks_Clone/Support/Extensions/ASDisplayNode+.swift
@@ -8,28 +8,32 @@
 import AsyncDisplayKit
 
 extension ASDisplayNode {
-    // MARK: - Set UIView's Shadow
-    func dropShadow(color: UIColor, offSet: CGSize, opacity: Float, blur: CGFloat) {
+    
+    // MARK: - Set ASDisplayNode's Shadow
+    
+    func dropShadow(color: UIColor, offSet: CGSize, opacity: CGFloat, blur: CGFloat) {
         // 그림자 색상 설정
-        self.layer.shadowColor = color.cgColor
+        self.shadowColor = color.cgColor
         // 그림자 크기 설정
-        self.layer.shadowOffset = offSet
+        self.shadowOffset = offSet
         // 그림자 투명도 설정
-        self.layer.shadowOpacity = opacity
+        self.shadowOpacity = opacity
         // 그림자의 blur 설정
-        self.layer.shadowRadius = blur
+        self.shadowRadius = blur
         // 구글링 해보세요!
-        self.layer.masksToBounds = false
+        self.clipsToBounds = false
     }
+    
+    // MARK: - Set ASDisplayNode's Radius
     
     func makeRounded(cornerRadius : CGFloat?){
         if let cornerRadius_ = cornerRadius {
-            self.layer.cornerRadius = cornerRadius_
+            self.cornerRadius = cornerRadius_
         }  else {
             // cornerRadius 가 nil 일 경우의 default
-            self.layer.cornerRadius = self.layer.frame.height / 2
+            self.cornerRadius = self.layer.frame.height / 2
         }
         
-        self.layer.masksToBounds = true
+        self.clipsToBounds = true
     }
 }

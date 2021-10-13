dbc0616bd2cb3b691e08a9f03ccbd28578c75bdb
wonseok <wurijang@gmail.com>
2021-10-14 01:13:24 +0900

    [feat] NSAttributeString 편의 함수 생성

--- StarBucks_Clone/StarBucks_Clone/Support/Global/Attr.swift
+++ StarBucks_Clone/StarBucks_Clone/Support/Global/Attr.swift
@@ -8,45 +8,9 @@
 import UIKit
 
 // MARK: Attributes
-
 public struct Attr {
-    static var twelveSM: [NSAttributedString.Key: Any] {
-        return [.font: UIFont.systemFont(ofSize: 12.0, weight: .semibold),
-                .foregroundColor: UIColor.black]
-    }
-    
-    static var twelveBoldGr: [NSAttributedString.Key: Any] {
-        return [.font: UIFont.systemFont(ofSize: 12.0, weight: .bold),
-                .foregroundColor: UIColor.seaweedGreen]
-    }
-    
-    static var fifteenReg: [NSAttributedString.Key: Any] {
-        return [.font: UIFont.systemFont(ofSize: 15.0, weight: .regular),
-                .foregroundColor: UIColor.black]
-    }
-    
-    static var seventeenSM: [NSAttributedString.Key: Any] {
-        return [.font: UIFont.systemFont(ofSize: 17.0, weight: .semibold),
-                .foregroundColor: UIColor.black]
-    }
-    
-    static var fourteenMedGr: [NSAttributedString.Key: Any] {
-        return [.font: UIFont.systemFont(ofSize: 14.0, weight: .medium),
-                .foregroundColor: UIColor.brownGrey]
-    }
-    
-    static var twentyThreeBold: [NSAttributedString.Key: Any] {
-        return [.font: UIFont.systemFont(ofSize: 23.0, weight: .bold),
-                .foregroundColor: UIColor.black]
-    }
-    
-    static var thirteenSM: [NSAttributedString.Key: Any] {
-        return [.font: UIFont.systemFont(ofSize: 13.0, weight: .semibold),
-                .foregroundColor: UIColor.black]
-    }
-    
-    static var twentyEightBold: [NSAttributedString.Key: Any] {
-        return [.font: UIFont.systemFont(ofSize: 28.0, weight: .bold),
-                .foregroundColor: UIColor.black]
+    /// weight, color 디폴트는 regular, black
+    static func setFont(size: CGFloat, weight: UIFont.Weight = .regular, color: UIColor = .black) -> [NSAttributedString.Key: Any] {
+        return [.font: UIFont.systemFont(ofSize: size, weight: weight), .foregroundColor: color]
     }
 }

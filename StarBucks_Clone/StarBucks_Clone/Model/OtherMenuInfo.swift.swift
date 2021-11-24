290619c096ec0a64c3acc39fb9bc472fb54b210a
wonseok <wurijang@gmail.com>
2021-11-24 22:11:32 +0900

    [feat] OtherTab 디테일 제외 완성

---
+++ StarBucks_Clone/StarBucks_Clone/Model/OtherMenuInfo.swift.swift
@@ -0,0 +1,31 @@
+//
+//  OtherMenuInfo.swift.swift
+//  StarBucks_Clone
+//
+//  Created by Wonseok Lee on 2021/11/24.
+//
+
+import Foundation
+
+let otherMenuInfoData: [[CardDetailInfo]] = [
+    [
+        CardDetailInfo(iconImgName: "icGift", menuTitle: "프리퀀시"),
+        CardDetailInfo(iconImgName: "icGift", menuTitle: "리워드"),
+        CardDetailInfo(iconImgName: "icGift", menuTitle: "쿠폰"),
+        CardDetailInfo(iconImgName: "icGift", menuTitle: "e-기프트 카드"),
+        CardDetailInfo(iconImgName: "icGift", menuTitle: "What's New"),
+        CardDetailInfo(iconImgName: "icGift", menuTitle: "알림"),
+        CardDetailInfo(iconImgName: "icGift", menuTitle: "히스토리"),
+        CardDetailInfo(iconImgName: "icGift", menuTitle: "전자영수증"),
+        CardDetailInfo(iconImgName: "icGift", menuTitle: "마이 스타벅스 리뷰"),
+    ],
+    [
+        CardDetailInfo(iconImgName: "icGift", menuTitle: "스토어 케어"),
+        CardDetailInfo(iconImgName: "icGift", menuTitle: "고객의 소리"),
+        CardDetailInfo(iconImgName: "icGift", menuTitle: "매장 정보"),
+    ],
+    [
+        CardDetailInfo(iconImgName: "icGift", menuTitle: "이용약관"),
+        CardDetailInfo(iconImgName: "icGift", menuTitle: "개인정보 처리 방침"),
+    ],
+]
